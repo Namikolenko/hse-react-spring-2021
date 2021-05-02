b05c884244495a47aefbc160638f7a961724912a
namikolenko <norofom@gmail.com>
2021-05-02 21:00:51 +0300

    [REFACTORING] Переделал класс Task в функцию, теперь у таски нет своего состояния отдельно

--- src/firstPage/components/Task.js
+++ src/firstPage/components/Task.js
@@ -1,21 +1,17 @@
 import React from "react";
 
-export class Task extends React.Component {
-    render() {
-        return (
-            <div className="content-item">
-                <div><h2>Задача {this.props.id}</h2></div>
-                <div className="content-inside">Задача <h4>"{this.props.name}"</h4></div>
-                <div className="content-inside">Описание задачи: {this.props.description}</div>
-                <div className="content-inside button-div">
-                    <button type="button" className="button-confirm" onClick={() => {
-                        this.props.onChangeCompleted(this.props.completed, this.props.index)
-                    }}>{{...this.props}.completed.toString()}
-                    </button>
-                </div>
+export const Task = (props) => {
+    return (
+        <div className="content-item">
+            <div><h2>Задача {props.id}</h2></div>
+            <div className="content-inside">Задача <h4>"{props.name}"</h4></div>
+            <div className="content-inside">Описание задачи: {props.description}</div>
+            <div className="content-inside button-div">
+                <button type="button" className="button-confirm" onClick={() => {
+                    props.onChangeCompleted(props.completed, props.index)
+                }}>{{...props}.completed.toString()}
+                </button>
             </div>
-        )
-    }
+        </div>
+    )
 }
-
-export default Task
